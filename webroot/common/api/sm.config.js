fbf5c1bd60633ad874612b779e10d70658f3700f
Chinmay Zanpure <czanpure@juniper.net>
2014-08-27 14:05:27 -0700

    Reverting the sm.config.js changes

--- webroot/common/api/sm.config.js
+++ webroot/common/api/sm.config.js
@@ -4,11 +4,11 @@
 
 var config = {};
 
-config.core_path = '/Users/czanpure/oc-sm/contrail-web-core';
+config.core_path = '/usr/src/contrail/contrail-web-core';
 
 config.sm = {};
-config.sm.server_ip = '10.84.17.1';
-config.sm.server_port = 8081;
+config.sm.server_ip = '127.0.0.1';
+config.sm.server_port = 9001;
 
 config.DFLT_SERVER_IP = '127.0.0.1';
 config.SM_API_SERVER = 'sm_api_server'
