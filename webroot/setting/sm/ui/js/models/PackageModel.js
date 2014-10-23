db8f307f94ff61b48e00d8d283c771a073bfdfbb
Chinmay Zanpure <czanpure@juniper.net>
2014-10-23 11:25:55 -0700

    Implemented Add Image, Delete Image, Add Package, Delete Package in Images, Packages --Added validations for add, delete

--- webroot/setting/sm/ui/js/models/PackageModel.js
+++ webroot/setting/sm/ui/js/models/PackageModel.js
@@ -4,23 +4,88 @@
 
 define([
     'underscore',
+    'knockback',
+    'knockout',
     'common/ui/js/models/ContrailModel'
-], function (_, ContrailModel) {
+], function (_, Knockback, Knockout, ContrailModel) {
     var PackageModel = ContrailModel.extend({
         defaultConfig: {
+            'id': null,
             'version': null,
             'path': null,
             'type': null,
             'parameters': {}
         },
-        configure: function (modalId) {
-            if (this.model().isValid(true)) {
-                // TODO: Check for form-level validation if required
+        configure: function (callback) {
+            var ajaxConfig = {};
+            if (this.model().isValid(true, "configureValidation")) {
                 if (true) {
-                    console.log(this.model().attributes);
-                    $("#" + modalId).modal('hide');
+                    var imageAttrs = this.model().attributes,
+                        putData = {}, images = [],
+                        that = this;
+
+                    images.push({
+                        'id'     : imageAttrs['id'],
+                        'version': imageAttrs['version'],
+                        'path'   : imageAttrs['path'],
+                        'type'   : imageAttrs['type']
+                    });
+                    putData[smConstants.IMAGE_PREFIX_ID] = images;
+
+                    ajaxConfig.type = "POST";
+                    ajaxConfig.data = JSON.stringify(putData);
+                    ajaxConfig.url = smUtils.getObjectUrl(smConstants.IMAGE_PREFIX_ID);
+
+                    contrail.ajaxHandler(ajaxConfig, function () {
+                    }, function (response) {
+                        console.log(response);
+                        if (contrail.checkIfFunction(callback)) {
+                            callback();
+                        }
+                    }, function (error) {
+                        console.log(error);
+                        that.showErrorAttr(smConstants.PACKAGE_PREFIX_ID + '_form', error['responseText']);
+                    });
+
                 } else {
                     // TODO: Show form-level error message if any
+                }
+            }
+        },
+        deletePackage: function (modalId, checkedRow, callback){
+            var ajaxConfig = {}, that = this,
+                clusterId = checkedRow['id'];
+            ajaxConfig.type = "DELETE";
+            ajaxConfig.url = '/sm/objects/image?id=' + clusterId;
+
+            contrail.ajaxHandler(ajaxConfig, function () {
+            }, function (response) {
+                console.log(response);
+                if (contrail.checkIfFunction(callback)) {
+                    callback();
+                }
+            }, function (error) {
+                console.log(error);
+                that.showErrorAttr('deletePackage', error.responseText);
+            });
+        },
+        validations: {
+            configureValidation: {
+                'id': {
+                    required: true,
+                    msg: smMessages.getRequiredMessage('id')
+                },
+                'type': {
+                    required: true,
+                    msg: smMessages.getRequiredMessage('type')
+                },
+                'version': {
+                    required: true,
+                    msg: smMessages.getRequiredMessage('version')
+                },
+                'path': {
+                    required: true,
+                    msg: smMessages.getRequiredMessage('path')
                 }
             }
         }
--- webroot/setting/sm/ui/js/views/ImageEditView.js
+++ webroot/setting/sm/ui/js/views/ImageEditView.js
@@ -8,7 +8,8 @@ define([
     'knockback'
 ], function (_, Backbone, Knockback) {
     var prefixId = smConstants.IMAGE_PREFIX_ID,
-        editTemplate = contrail.getTemplate4Id("sm-edit-form-template");
+        editTemplate = contrail.getTemplate4Id("sm-edit-form-template"),
+        modalId = 'configure-' + prefixId;
 
     var ImageEditView = Backbone.View.extend({
         render: function (options) {
@@ -17,18 +18,44 @@ define([
                 that = this;
 
             smUtils.createModal({'modalId': modalId, 'className': 'modal-700', 'title': options['title'], 'body': editLayout, 'onSave': function () {
-                that.model.configure(modalId); // TODO: Release binding on successful configure
+                that.model.configure(function () {
+                    options['callback']();
+                    $('#' + modalId).modal('hide');
+                });
             }, 'onCancel': function () {
                 Knockback.release(that.model, document.getElementById(modalId));
                 smValidation.unbind(that);
                 $("#" + modalId).modal('hide');
             }});
 
-            smUtils.renderView4Config($("#" + modalId).find("#sm-" + prefixId + "-form"), this.model, configureViewConfig);
+            smUtils.renderView4Config($("#" + modalId).find("#sm-" + prefixId + "-form"), this.model, configureViewConfig, "configureValidation");
+
+            this.model.showErrorAttr(prefixId + '_form', false);
+            Knockback.applyBindings(this.model, document.getElementById(modalId));
+            smValidation.bind(this);
+        },
+        renderDeleteImage: function (options) {
+            var textTemplate = contrail.getTemplate4Id("sm-delete-image-template"),
+                elId = 'deleteImage',
+                that = this,
+                checkedRows = options['checkedRows'],
+                ImageToBeDeleted = {'imageId': [], 'elementId': elId};
+            ImageToBeDeleted['imageId'].push(checkedRows['id']);
+            smUtils.createModal({'modalId': modalId, 'className': 'modal-700', 'title': options['title'], 'body': textTemplate(ImageToBeDeleted), 'onSave': function () {
+                that.model.deleteImage(modalId, options['checkedRows'], function(){
+                    options['callback']();
+                    $('#' + modalId).modal('hide');
+                });
+            }, 'onCancel': function () {
+                $("#" + modalId).modal('hide');
+            }});
+
+            this.model.showErrorAttr(elId, false);
 
             Knockback.applyBindings(this.model, document.getElementById(modalId));
             smValidation.bind(this);
         }
+
     });
 
     var configureViewConfig = {
@@ -38,13 +65,14 @@ define([
             rows: [
                 {
                     columns: [
+                        {elementId: 'id', view: "FormInputView", viewConfig: {path: "id", dataBindValue: "id", class: "span6"}},
                         {elementId: 'type', view: "FormInputView", viewConfig: {path: "type", dataBindValue: "type", class: "span6"}},
-                        {elementId: 'version', view: "FormInputView", viewConfig: {path: 'version', dataBindValue: "version", class: "span6"}}
                     ]
                 },
                 {
                     columns: [
-                        {elementId: 'path', view: "FormInputView", viewConfig: {path: "path", dataBindValue: "path", class: "span12"}}
+                        {elementId: 'version', view: "FormInputView", viewConfig: {path: 'version', dataBindValue: "version", class: "span6"}},
+                        {elementId: 'path', view: "FormInputView", viewConfig: {path: "path", dataBindValue: "path", class: "span6"}}
                     ]
                 }
             ]
