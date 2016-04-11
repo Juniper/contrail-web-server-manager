0945d1a1686d29838ebd52f7326b16a704b71077
Chinmay Zanpure <czanpure@juniper.net>
2016-04-11 13:58:24 -0700

    Closes-Bug: #1560691 Added transaction logs for get/put/post/delete operations for the webServer logs.
    Change-Id: Ie813dc8be66baeea83b0f8436d0c3c710ff90ec9

--- webroot/common/api/sm.js
+++ webroot/common/api/sm.js
@@ -3,7 +3,8 @@
  */
 
 
-var  rest = require(process.mainModule.exports["corePath"] + '/src/serverroot/common/rest.json.api');
+var rest = require(process.mainModule.exports["corePath"] + '/src/serverroot/common/rest.json.api'),
+    logutils = require(process.mainModule.exports["corePath"] + '/src/serverroot/utils/log.utils');
 
 var smConfig = require('../../common/api/sm.config'),
     smConstants = require('../../common/api/sm.constants'),
@@ -20,25 +21,33 @@ var smREST = rest.getAPIServer({
 });
 
 function get(url, callback) {
+    logutils.logger.debug('TRANSACTION - ACTION - GET - ' + url);
     smREST.api.get(url, function (err, data) {
+        logutils.logger.info('TRANSACTION - ACTION - GET - RESPONSE: ' + JSON.stringify(data));
         callback(err, data);
     });
 }
 
 function put(url, putData, appData, callback) {
+    logutils.logger.debug('TRANSACTION - ACTION - PUT - ' + url + ' PUT DATA: ' + JSON.stringify(putData));
     smREST.api.put(url, putData, function (err, data) {
+        logutils.logger.debug('TRANSACTION - ACTION - PUT - RESPONSE: ' + JSON.stringify(data));
         callback(err, data);
     });
 }
 
 function post(url, postData, appData, callback) {
+    logutils.logger.debug('TRANSACTION - ACTION - POST - ' + url + ' POST DATA: ' + JSON.stringify(postData));
     smREST.api.post(url, postData, function (err, data) {
-        callback(err, data);
+        logutils.logger.debug('TRANSACTION - ACTION - POST - RESPONSE: ' + JSON.stringify(data));
+        callback(err, data);s
     });
 }
 
 function del(url, appData, callback) {
+    logutils.logger.debug('TRANSACTION - ACTION - DELETE - ' + url);
     smREST.api.delete(url, function (err, data) {
+        logutils.logger.debug('TRANSACTION - ACTION - DELETE - RESPONSE: ' + JSON.stringify(data));
         callback(err, data);
     });
 }
