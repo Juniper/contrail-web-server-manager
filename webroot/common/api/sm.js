3bf86e88e91c06464b8dbb3cf811362e9df4435a
ZheyangSong <jerome_soung@hotmail.com>
2019-02-01 15:51:11 -0800

    closes-jira-bug:JCB-168262
    Hide Sensitive Data from Log Files.
    
    Change-Id: I8441a9a07bf0cfc1c10fb579e3f7cd6e7ce72cf0

--- webroot/common/api/sm.js
+++ webroot/common/api/sm.js
@@ -19,26 +19,54 @@ var smREST = rest.getAPIServer({
     port: smServerPort
 });
 
+var SANITIZE_KEYS = [
+    "impi_password",
+    "adminPass",
+    "admin_pass",
+    "password",
+    "admin_password",
+    "admin_key",
+    "storage_mon_secret",
+    "keystone_password"
+];
+var SANITIZE_PATTERNS = [];
+
+function patternStr(key) {
+    return '"(.*?' + key + ')":"([^"]*)"';
+};
+
+for (var i = 0, iLen = SANITIZE_KEYS.length; i < iLen; i++) {
+    SANITIZE_PATTERNS.push(new RegExp(patternStr(SANITIZE_KEYS[i]), "img"));
+}
+
+function sanitizedDataString(data) {
+    return SANITIZE_PATTERNS.reduce(function (prevDataStr, pattern) {
+        return prevDataStr.replace(pattern, function (_, key) {
+            return ['"', key, '":"<hidden from log>"'].join();
+        });
+    }, JSON.stringify(data));
+}
+
 function get(url, callback) {
     logutils.logger.debug("TRANSACTION - ACTION - GET - " + url);
     smREST.api.get(url, function (err, data) {
-        logutils.logger.info("TRANSACTION - ACTION - GET - RESPONSE: " + JSON.stringify(data));
+        logutils.logger.info("TRANSACTION - ACTION - GET - RESPONSE: " + sanitizedDataString(data));
         callback(err, data);
     });
 }
 
 function put(url, putData, appData, callback) {
-    logutils.logger.debug("TRANSACTION - ACTION - PUT - " + url + " PUT DATA: " + JSON.stringify(putData));
+    logutils.logger.debug("TRANSACTION - ACTION - PUT - " + url + " PUT DATA: " + sanitizedDataString(putData));
     smREST.api.put(url, putData, function (err, data) {
-        logutils.logger.debug("TRANSACTION - ACTION - PUT - RESPONSE: " + JSON.stringify(data));
+        logutils.logger.debug("TRANSACTION - ACTION - PUT - RESPONSE: " + sanitizedDataString(data));
         callback(err, data);
     });
 }
 
 function post(url, postData, appData, callback) {
-    logutils.logger.debug("TRANSACTION - ACTION - POST - " + url + " POST DATA: " + JSON.stringify(postData));
+    logutils.logger.debug("TRANSACTION - ACTION - POST - " + url + " POST DATA: " + sanitizedDataString(postData));
     smREST.api.post(url, postData, function (err, data) {
-        logutils.logger.debug("TRANSACTION - ACTION - POST - RESPONSE: " + JSON.stringify(data));
+        logutils.logger.debug("TRANSACTION - ACTION - POST - RESPONSE: " + sanitizedDataString(data));
         callback(err, data);
     });
 }
@@ -46,7 +74,7 @@ function post(url, postData, appData, callback) {
 function del(url, appData, callback) {
     logutils.logger.debug("TRANSACTION - ACTION - DELETE - " + url);
     smREST.api.delete(url, function (err, data) {
-        logutils.logger.debug("TRANSACTION - ACTION - DELETE - RESPONSE: " + JSON.stringify(data));
+        logutils.logger.debug("TRANSACTION - ACTION - DELETE - RESPONSE: " + sanitizedDataString(data));
         callback(err, data);
     });
 }
